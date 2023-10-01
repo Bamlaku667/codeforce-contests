a115ea52a3ecf9cd099cd935528492b5abca692f
Bamlaku <bamlakhiruya@gmail.com>
2023-10-01 11:05:14 +0300

    solving aleksa and stack problem

--- B_Aleksa_and_Stack.py
+++ B_Aleksa_and_Stack.py
@@ -1,6 +1,27 @@
+import random 
 if __name__ == "__main__":
     t = int(input())
     for _ in range(t):
-        n = map(int, input())
-        print(n)
+        n = int(input())
+        a1 = random.randint(3, 10**9)
+        a2 = random.randint(a1+1, 10**9 )
+        arr = [a1, a2]
+        
+        i = 2
+        
+        while i < n:
+            current = arr[-1] + 1
+            while (3 * current) % (arr[i-1] + arr[i -2]) == 0 or current <= arr[i -1]:
+                current += 1
+                
+            arr.append(current)
+            
+            i += 1
+            
+        for num in arr:
+            print(num , end = " ")
+            
+            
+
+
 
